a585923407cc29cfd61d62ab2b7c4560e84673e2
Veikka Kähkönen <insiding@gmail.com>
2018-11-03 17:12:19 +0200

    Add basic boilerplate

---
+++ build-utils/webpack.common.js
@@ -0,0 +1,43 @@
+const commonPaths = require('./common-paths');
+
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const config = {
+  output: {
+    path: commonPaths.outputPath,
+    publicPath: '/'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          chunks: 'initial',
+          test: 'vendor',
+          name: 'vendor',
+          enforce: true
+        }
+      }
+    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: `public/index.html`,
+      favicon: `public/favicon.ico`
+    })
+  ]
+};
+
+module.exports = config;
---
+++ src/components/App.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+
+const App = () => <div className="app">foo</div>;
+
+export default App;
