1f32f6690ee6b86b44dd398e114adf4873a61c9f
Veikka Kähkönen <insiding@gmail.com>
2018-11-04 16:21:48 +0200

    Add shader execution

--- src/components/ShaderLoader.tsx
+++ src/components/ShaderLoader.tsx
@@ -1,20 +1,37 @@
 import * as React from 'react';
 
-import { init as initGl } from '../lib/gl';
+import { init as initGl, createShader, linkProgram, setupShadedFullScreenTriangle } from '../lib/gl';
 import { GlContextInterface } from '../contexts/GlContext';
 import withGlContext from './withGlContext';
+import commonVs from '../shaders/common.vs.glsl';
+import testFs from '../shaders/test.fs.glsl';
 
-export interface ShaderLoaderProps {
-  gl: GlContextInterface,
+console.log(commonVs);
+
+export interface ShaderLoaderProps extends GlContextInterface {
 }
 
 class ShaderLoader extends React.Component<ShaderLoaderProps, any> {
+  state = {
+    vertexShader: null,
+  };
+
   componentDidMount() {
     const { getWebGlContext } = this.props;
 
-    const glContext = getWebGlContext();
+    const gl = getWebGlContext();
+    if (gl === null) {
+      throw new Error('WebGL unavailable');
+    }
+    initGl(gl);
 
-    initGl(glContext);
+    const vertexShader = createShader(gl, gl.VERTEX_SHADER, commonVs);
+    const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, testFs);
+    let program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    linkProgram(gl, program);
+    setupShadedFullScreenTriangle(gl, program);
   }
 
   render() {
--- src/lib/gl.ts
+++ src/lib/gl.ts
@@ -1,8 +1,70 @@
-export const init = (gl: WebGLRenderingContext | null) => {
-  if (gl === null) {
-    throw new Error('WebGL unavailable');
-  }
+type RenderingContext = WebGLRenderingContext | WebGL2RenderingContext;
 
+export const init = (gl: RenderingContext) => {
   gl.clearColor(0, 0, 0, 1);
   gl.clear(gl.COLOR_BUFFER_BIT);
+}
+
+export const createShader = (
+  gl: RenderingContext,
+  shaderType: number,
+  source: string,
+) : WebGLShader => {
+  const shader = gl.createShader(shaderType);
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+
+  const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+  if (success && shader) {
+    return shader;
+  }
+
+  const error = gl.getShaderInfoLog(shader);
+  gl.deleteShader(shader);
+  if (error) {
+    throw new Error(error);
+  } else {
+    throw new Error('Unknown shader compilation error');
+  }
+}
+
+export const linkProgram = (
+  gl: RenderingContext,
+  program: WebGLProgram | null,
+) : WebGLProgram => {
+  gl.linkProgram(program);
+  const success = gl.getProgramParameter(program, gl.LINK_STATUS);
+  if (success && program) {
+    return program;
+  }
+
+  const error = gl.getProgramInfoLog(program);
+  gl.deleteProgram(program);
+  if (error) {
+    throw new Error(error);
+  } else {
+    throw new Error('Unknown shader linking error');
+  }
+}
+
+export const setupShadedFullScreenTriangle = (
+  gl: RenderingContext,
+  program: WebGLProgram,
+) : void => {
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+    const positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+    const positionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    const triangle = [
+      -1, -1,
+      3, -1,
+      -1, 3,
+    ];
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangle), gl.STATIC_DRAW);
+
+    gl.useProgram(program);
+    gl.enableVertexAttribArray(positionAttributeLocation);
+    gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
+    gl.drawArrays(gl.TRIANGLES, 0, 3);
 }
